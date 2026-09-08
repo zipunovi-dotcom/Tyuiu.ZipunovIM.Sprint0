9eed7c9ef399377cabdeb63f4a94a7d972e9710c
Igor Zipunov <zipunovi@gmail.com>
2026-09-08 23:49:26 +0500

    Task1

---
+++ Tyuiu.ZipunovIM.Sprint0.Task1.V0/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Net.Cache;
+
+Console.WriteLine("Hello, World!");
+String name = "Игорь";
+int age = 18;
+Console.WriteLine("Здравствуйте, меня зовут " + name + " мне " + age + " Лет! ");
+Console.ReadKey ();
