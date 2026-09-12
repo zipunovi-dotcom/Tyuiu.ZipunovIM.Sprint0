7e2e72abacf84d20adf34e1e900d3a5d7b7c572b
Igor Zipunov <zipunovi@gmail.com>
2026-09-12 16:03:40 +0500

    Task5

---
+++ Tyuiu.ZipunovIM.Sprint0.Task5.V0.Test/DataServiceTest.cs
@@ -0,0 +1,15 @@
+namespace Tyuiu.ZipunovIM.Sprint0.Task5.V0.Test;
+using Tyuiu.ZipunovIM.Sprint0.Task5.V0.Lib;
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
---
+++ Tyuiu.ZipunovIM.Sprint0.Task5.V0/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Numerics;
+using Tyuiu.ZipunovIM.Sprint0.Task5.V0.Lib;
+Console.WriteLine("A + B =" + DataService.Addition(1, 5));
+Console.WriteLine("A / B =" + DataService.Division(9, 3));
+Console.WriteLine("A - B =" + DataService.Subtraction(15, 5));
+Console.WriteLine("A * B =" + DataService.Multiplication (10, 10));
+Console.ReadKey();
